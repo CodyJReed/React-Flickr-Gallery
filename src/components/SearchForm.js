5164a5bb4a7edb28e8b870e6f8d552d9b417ee8a
Cody Reed <cody2j2reed@gmail.com>
2018-07-23 13:40:31 -0400

    install react bootstrap and modify index.css

---
+++ src/components/SearchForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  Button
+} from 'react-bootstrap';
+
+const SearchForm = () => (
+  <Form inline>
+    <FormGroup controlId="formInlineEmail">
+      <FormControl type="search" placeholder="Search" />
+    </FormGroup>
+    {' '}
+    <Button type="submit">
+      search
+    </Button>
+  </Form>
+
+);
+
+export default SearchForm;
